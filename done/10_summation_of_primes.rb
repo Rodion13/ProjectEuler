ad12b22a7563cbd18cb15970528b1e1e79a49705
Rodion Yaryy <ryaryy@gmail.com>
2015-01-06 17:30:06 -0800

    problem #10 solution

---
+++ done/10_summation_of_primes.rb
@@ -0,0 +1,53 @@
+# Summation of primes
+# Problem 10
+# The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+# Find the sum of all the primes below two million.
+
+# Rodion Yaryy
+# https://projecteuler.net/problem=10
+
+require 'prime'
+beginning = Time.now
+
+def prime?(number)
+  2.upto(Math.sqrt(number).to_i).each do |i|
+    return false if number % i == 0
+  end
+  return true
+end
+
+sum = 2
+
+(3..2000000).each do |i|
+  if prime?(i) then sum += i end
+end
+
+puts sum
+puts "Time elapsed #{Time.now - beginning} seconds."
+# Time elapsed 16.425839 seconds.
+
+
+######### ######### ######### ######### ######### ######### ######### ######### ######### ######### #########
+
+beginning = Time.now
+sum2 =0
+
+def prime_fast?(n)
+  nums = [nil, nil, *2..n]
+  (2..Math.sqrt(n)).each do |i|
+    (i**2..n).step(i){|m| nums[m] = nil}  if nums[i]
+  end
+  nums.compact
+end
+
+prime_fast?(2000000).each do |i| sum2 += i end
+
+puts sum2
+puts "Time elapsed #{Time.now - beginning} seconds."
+
+# Time elapsed 0.545806 seconds.
+
+######### ######### ######### ######### ######### ######### ######### ######### ######### ######### #########
+
+
+
